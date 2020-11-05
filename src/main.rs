89226c71ba41192a566ad714ae65e28010f659ed
むずでょ <muzudho1@gmail.com>
2020-11-06 01:09:00 +0900

    Sequence3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 pub mod sequence1;
 pub mod sequence2;
+pub mod sequence3;
 
 use crate::sequence1::Sequence1;
 use crate::sequence2::Sequence2;
+use crate::sequence3::Sequence3;
 
 fn main() {
     sub1();
     sub2();
+    sub3();
 }
 
 fn sub1() {
@@ -47,4 +50,23 @@ fn sub2() {
         format!("{:?}", &seq)
     );
     println!("Finished: sub2.");
+}
+
+fn sub3() {
+    println!("Start: sub3.");
+    let seq = Sequence3 {
+        sequence: vec![
+            'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '!',
+        ],
+    };
+
+    // println!("{:?}", &seq.next());
+
+    // Hello, World!! の１文字ずつをシングルクォーテーションで囲んだもの。
+    println!("{:?}", &seq);
+    assert_eq!(
+        "'H''e''l''l''o'','' ''W''o''r''l''d''!''!'",
+        format!("{:?}", &seq)
+    );
+    println!("Finished: sub3.");
 }
---
+++ src/sequence3.rs
@@ -0,0 +1,58 @@
+use std::fmt;
+
+/// イテレートできるもの。  
+#[derive(Clone)]
+pub struct Sequence3 {
+    /// String型は Unicodeなのでイテレートが案外難しい。 `Vec<char>` にしたのが工夫。  
+    pub sequence: Vec<char>,
+}
+
+impl Sequence3 {
+    pub fn cursor(&self) -> Sequence3Cursor {
+        Sequence3Cursor {
+            // ここで .clone() 要る。意味ない。イテレートするたびに .clone() するのは重くないか？
+            owner: Box::new(self.clone()),
+            curr: 0,
+        }
+    }
+}
+
+/// カーソルを分離。
+pub struct Sequence3Cursor {
+    pub owner: Box<Sequence3>,
+    /// イテレートで使う配列のインデックスのようなもの。  
+    /// しかし `&mut self` でないと イテレートできない(`iter_mut()相当`)よな。  
+    pub curr: usize,
+}
+
+/// 普段よく見る `for item in &items {` イテレーションの下地となる、  
+/// 普段あまり表に出てこない `items.next()` を使ったイテレーションの実装です。  
+impl Iterator for Sequence3Cursor {
+    // Self::Item ってこれ。
+    type Item = Box<char>;
+
+    // The return type is `Option<T>`:
+    //     * When the `Iterator` is finished, `None` is returned.
+    //     * Otherwise, the next value is wrapped in `Some` and returned.
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.curr < self.owner.sequence.len() {
+            // .clone() するよりは Box でラッピングした方がいいだろうか？
+            let item = Some(Box::new(self.owner.sequence[self.curr]));
+            self.curr += 1;
+            return item;
+        }
+
+        return None;
+    }
+}
+
+/// デバッグ出力。  
+impl fmt::Debug for Sequence3 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut buf = String::new();
+        for chr in self.cursor() {
+            buf.push_str(&format!("{:?}", chr));
+        }
+        write!(f, "{}", buf)
+    }
+}
