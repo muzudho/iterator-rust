d2cdf4c7c3bfdfa40d9649799c991f90f5ccf81d
むずでょ <muzudho1@gmail.com>
2020-11-06 00:47:10 +0900

    sequence2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 pub mod sequence1;
+pub mod sequence2;
 
 use crate::sequence1::Sequence1;
+use crate::sequence2::Sequence2;
 
 fn main() {
-    println!("Start.");
+    sub1();
+    sub2();
+}
 
+fn sub1() {
+    println!("Start: sub1.");
     let seq = Sequence1 {
         sequence: vec![
             'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '!',
@@ -20,6 +26,25 @@ fn main() {
         "'H''e''l''l''o'','' ''W''o''r''l''d''!''!'",
         format!("{:?}", &seq)
     );
+    println!("Finished: sub1.");
+}
 
-    println!("Finished.");
+fn sub2() {
+    println!("Start: sub2.");
+    let seq = Sequence2 {
+        sequence: vec![
+            'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '!',
+        ],
+        curr: 0,
+    };
+
+    // println!("{:?}", &seq.next());
+
+    // Hello, World!! の１文字ずつをシングルクォーテーションで囲んだもの。
+    println!("{:?}", &seq);
+    assert_eq!(
+        "'H''e''l''l''o'','' ''W''o''r''l''d''!''!'",
+        format!("{:?}", &seq)
+    );
+    println!("Finished: sub2.");
 }
